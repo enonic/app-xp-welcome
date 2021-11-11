69eb3fa3f6d7162efdf09f6a929b88526341b051
Alan Semenov <ase@enonic.com>
2021-11-11 17:16:45 +0100

    Applied makeup to the Welcome page (#7)

--- src/main/resources/welcome/welcome.js
+++ src/main/resources/welcome/welcome.js
@@ -1,4 +1,6 @@
+const auth = require('/lib/xp/auth');
 const admin = require('/lib/xp/admin');
+const portal = require('/lib/xp/portal');
 const mustache = require('/lib/mustache');
 
 const bean = __.newBean('com.enonic.xp.app.welcome.WelcomePageScriptBean');
@@ -15,13 +17,21 @@ exports.get = function () {
     let view = resolve('./welcome.html');
     let webApplications = getWebApplications();
     let siteDetails = getSiteDetails();
+
     let params = {
         xpVersion: admin.getVersion(),
         applications: webApplications.applications,
         managementApiUrl: bean.getManagementApiUrl(),
         statisticsApiUrl: bean.getStatisticsApiUrl(),
         xpUrl: bean.getXpUrl(),
-        sites: siteDetails.sites
+        hasSites: siteDetails.sites.length > 0,
+        hasApplications: webApplications.applications.length > 0,
+        sites: siteDetails.sites,
+        assetsUri: portal.assetUrl({
+            path: '',
+            application: app.name
+        }),
+        isLoggedIn: !!auth.getUser()
     };
     return {
         contentType: 'text/html',
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const ErrorLoggerPlugin = require('error-logger-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    context: path.join(__dirname, '/src/main/resources/assets'),
+    entry: {
+        main: './js/main.js'
+    },
+    output: {
+        path: path.join(__dirname, '/build/resources/main/assets/'),
+        filename: './js/_all.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader, options: {publicPath: '../'}},
+                    {loader: 'css-loader', options: {sourceMap: !isProd, importLoaders: 1, url: false}},
+                    {loader: 'postcss-loader', options: { sourceMap: !isProd} },
+                    {loader: 'less-loader', options: {sourceMap: !isProd}},
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: false
+                    },
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+            })
+        ]
+    },
+    plugins: [
+        new ErrorLoggerPlugin(),
+        new MiniCssExtractPlugin({
+            filename: './styles/_all.css',
+            chunkFilename: './styles/_all.css'
+        })
+    ],
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? false : 'source-map'
+};
