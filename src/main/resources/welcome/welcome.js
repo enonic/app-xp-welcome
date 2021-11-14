5a06ac8bbd1e9c41e6d74d7762250b446459d735
Alan Semenov <ase@enonic.com>
2021-11-14 12:03:36 +0100

    Prepare for localisation (#13)

--- src/main/resources/welcome/welcome.js
+++ src/main/resources/welcome/welcome.js
@@ -1,6 +1,7 @@
 const auth = require('/lib/xp/auth');
 const admin = require('/lib/xp/admin');
 const portal = require('/lib/xp/portal');
+const i18nLib = require('/lib/xp/i18n');
 const mustache = require('/lib/mustache');
 
 const bean = __.newBean('com.enonic.xp.app.welcome.WelcomePageScriptBean');
@@ -12,13 +13,19 @@ function getWebApplications() {
 function getSiteDetails() {
     return __.toNativeObject(bean.getSites());
 }
-
 exports.get = function () {
-    let view = resolve('./welcome.html');
-    let webApplications = getWebApplications();
-    let siteDetails = getSiteDetails();
+    const view = resolve('./welcome.html');
+    const webApplications = getWebApplications();
+    const siteDetails = getSiteDetails();
 
-    let params = {
+    const locales = admin.getLocales();
+    log.info('Locales: ' + JSON.stringify(locales, null, 4));
+
+    const supportedLocales = i18nLib.getSupportedLocales(['i18n/phrases']);
+    log.info('Bundle locales: ' + JSON.stringify(supportedLocales, null, 4));
+    const phrases = i18nLib.getPhrases(locales, ['i18n/phrases']);
+
+    const params = {
         xpVersion: admin.getVersion(),
         applications: webApplications.applications,
         managementApiUrl: bean.getManagementApiUrl(),
@@ -31,7 +38,10 @@ exports.get = function () {
             path: '',
             application: app.name
         }),
-        isLoggedIn: !!auth.getUser()
+        isLoggedIn: !!auth.getUser()/*,
+        i18n: {
+            "page.title": phrases['page.title']
+        }*/
     };
     return {
         contentType: 'text/html',
