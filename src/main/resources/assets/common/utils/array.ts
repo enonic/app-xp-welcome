b4ffe7098b9213be9bdf610c9547b749d6d75f13
Mikita Taukachou <edloidas@gmail.com>
2023-06-09 17:31:20 +0300

    Enable Log in as a guest #85
    Made Login button blue, if user is logged in.
    Disabled projects filtering to return the "default" project too.

--- src/main/resources/assets/common/utils/array.ts
+++ src/main/resources/assets/common/utils/array.ts
@@ -1,7 +1,9 @@
-export function kebab<T, J>(array: T[], joiner: (index: number) => J): (T | J)[];
+type JoinerCreator<J> = (index: number) => J;
+
+export function kebab<T, J>(array: T[], joiner: JoinerCreator<J>): (T | J)[];
 export function kebab<T, J>(array: T[], joiner: J): (T | J)[] {
     return array.reduce((r: (T | J)[], a: T, index) => {
-        const j = typeof joiner === 'function' ? joiner(index) : joiner;
+        const j = typeof joiner === 'function' ? (joiner as JoinerCreator<J>)(index) : joiner;
         return index === 0 ? r.concat(a) : r.concat(j, a);
     }, []);
 }
