667d41f6248c8fbcc06996c4f11ac3ea407b409f
Pavel Milkevich <pmi@enonic.com>
2023-10-17 10:01:29 +0200

    Added StoreBeanTest

---
+++ src/test/java/com/enonic/xp/app/welcome/StoreBeanTest.java
@@ -0,0 +1,40 @@
+package com.enonic.xp.app.welcome;
+
+import org.junit.jupiter.api.Test;
+
+import com.enonic.xp.testing.ScriptTestSupport;
+
+public class StoreBeanTest
+    extends ScriptTestSupport
+{
+
+    @Test
+    public void testPutGet()
+    {
+        runFunction( "/test/StoreBeanTest.js", "testPutGet" );
+    }
+
+    @Test
+    public void testGetNotExists()
+    {
+        runFunction( "/test/StoreBeanTest.js", "testGetNotExists" );
+    }
+
+    @Test
+    public void testPutRemove()
+    {
+        runFunction( "/test/StoreBeanTest.js", "testPutRemove" );
+    }
+
+    @Test
+    public void testGetByUrl()
+    {
+        runFunction( "/test/StoreBeanTest.js", "testGetByUrl" );
+    }
+
+    @Test
+    public void testGetByUrlNotExists()
+    {
+        runFunction( "/test/StoreBeanTest.js", "testGetByUrlNotExists" );
+    }
+}
